8b64462cec54231a5a6d468f2ccb62761365e505
Nat Bennett <natalie.j.bennett@gmail.com>
2023-07-08 18:17:29 -0700

    Fix 500 error on download by moving the file into a heredoc
    This guarantees that the contents of the file
    will be accessible
    from within the Dockerized version of the app

--- lib/nvim_config_gen_web/controllers/page_controller.ex
+++ lib/nvim_config_gen_web/controllers/page_controller.ex
@@ -8,7 +8,46 @@ defmodule NvimConfigGenWeb.PageController do
   end
 
   def download(conn, _params) do
-    { :ok, contents } = File.read("./assets/init.lua")
-    send_download(conn, {:binary, contents}, filename: "init.lua")
+    send_download(conn, {:binary, contents()}, filename: "init.lua")
+  end
+
+  defp contents() do
+"""
+--- bootstrap plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+--- load plugins
+plugins = {
+  { 'AlexvZyl/nordic.nvim' }
+}
+require("lazy").setup(plugins)
+
+--- this stops Nvim from adding a tab
+--- when it autoindents a line
+--- in addition to the spaces its copying from the above line
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+--- set line numbers for pairing
+vim.opt.number = true
+
+--- enable 24-bit RGB colors
+vim.opt.termguicolors = true
+
+--- set the colorscheme
+vim.cmd("colorscheme nordic")
+"""
   end
 end
